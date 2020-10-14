d9c3c732a9ccb377f99d55ba36bdc82446d1c6ac
Karen Nikoghosyan <63981352+KarenNikoghosyan@users.noreply.github.com>
2020-10-14 20:01:40 +0300

    V.0.01
    Basic bird movement.
    Added bottom and top colliders.

---
+++ Flappy_Bird/Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionHandler : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Environment")
+        {
+            Destroy(gameObject);
+            SceneManager.LoadScene(0); // todo remove this line of code
+        }
+    }
+
+}
---
+++ Flappy_Bird/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float jumpForce = 6f;
+    Rigidbody rigidBody;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        RespondToJumpInput();
+    }
+
+    private void RespondToJumpInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        rigidBody.velocity = Vector3.zero;
+        rigidBody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }   
+}
