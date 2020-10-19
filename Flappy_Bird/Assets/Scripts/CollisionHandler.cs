f8b4d222bdd1a78bdd778663b3766e3dd97d8560
Karen Nikoghosyan <63981352+KarenNikoghosyan@users.noreply.github.com>
2020-10-19 19:10:36 +0300

    V.0.16
    Added score UI and logic.

--- Flappy_Bird/Assets/Scripts/CollisionHandler.cs
+++ Flappy_Bird/Assets/Scripts/CollisionHandler.cs
@@ -1,18 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    [SerializeField] TextMeshProUGUI pipeScore;
+    int score = 0;
+
+    void Start()
+    {
+        pipeScore.text = score.ToString();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Environment")
         {
-            Destroy(gameObject, 1f);
-            SceneManager.LoadScene(0); // todo remove this line of code
+            KillPlayer();
+        }
+
+        if (other.gameObject.tag == "Score")
+        {
+            AddScore();
         }
     }
 
+    private void KillPlayer()
+    {
+        Destroy(gameObject, 1f);
+        SceneManager.LoadScene(0); // todo remove this line of code
+    }
+
+    private void AddScore()
+    {
+        score++;
+        pipeScore.text = score.ToString();
+    }
 }
