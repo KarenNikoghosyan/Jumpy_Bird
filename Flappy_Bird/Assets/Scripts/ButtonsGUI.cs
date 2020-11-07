2be87a8a62894b850ad757cb9fba08c09d13f7b8
Karen Nikoghosyan <63981352+KarenNikoghosyan@users.noreply.github.com>
2020-11-07 12:42:40 +0200

    V.0.59
    Added first iteration of UI pause button.
    Water splash particles -
    Reduced number of particles from 75 to 60.
    Reduced the height of the effect.
    Added UI asset pack.

---
+++ Flappy_Bird/Assets/Scripts/ButtonsGUI.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonsGUI : MonoBehaviour
+{
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+    }
+
+}
