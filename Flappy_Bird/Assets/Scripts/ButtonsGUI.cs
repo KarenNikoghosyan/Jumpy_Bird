fa05a9adf31b137a3504155559515129c36d952a
Karen Nikoghosyan <63981352+KarenNikoghosyan@users.noreply.github.com>
2020-11-07 13:55:49 +0200

    V.0.59.1
    Added sound pause effect.
    Stopping theme music when pressing pause button.

--- Flappy_Bird/Assets/Scripts/ButtonsGUI.cs
+++ Flappy_Bird/Assets/Scripts/ButtonsGUI.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class ButtonsGUI : MonoBehaviour
 {
+    AudioSource musicPlayer;
+
     public void PauseGame()
     {
+        AudioManager.instance.Play("Pause Sound");
+        musicPlayer = GameObject.Find("Music Player").GetComponent<AudioSource>();
+        musicPlayer.Stop();
         Time.timeScale = 0;
     }
 
