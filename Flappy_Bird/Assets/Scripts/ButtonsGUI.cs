14edd25b295d6c7ddfb65b5914041720a6a61b6f
Karen Nikoghosyan <63981352+KarenNikoghosyan@users.noreply.github.com>
2020-11-07 17:50:46 +0200

    V.0.60
    Added in game menu -
    Resume button was added.
    - more buttons will be added
    Added sound effects.

--- Flappy_Bird/Assets/Scripts/ButtonsGUI.cs
+++ Flappy_Bird/Assets/Scripts/ButtonsGUI.cs
@@ -5,13 +5,35 @@ using UnityEngine;
 public class ButtonsGUI : MonoBehaviour
 {
     AudioSource musicPlayer;
+    [SerializeField] Canvas inGameMenu , inGame;
+
+    bool isEnabled = false;
+
+    void Awake()
+    {
+        inGameMenu.enabled = false;
+        musicPlayer = GameObject.Find("Music Player").GetComponent<AudioSource>();
+    }
 
     public void PauseGame()
     {
         AudioManager.instance.Play("Pause Sound");
-        musicPlayer = GameObject.Find("Music Player").GetComponent<AudioSource>();
-        musicPlayer.Stop();
+        musicPlayer.Pause();
         Time.timeScale = 0;
+        inGameMenu.enabled = true;
+        inGame.enabled = false;
+        isEnabled = true;
     }
 
+    public void ResumeGame()
+    {
+        if (isEnabled) {
+            AudioManager.instance.Play("Resume Sound");
+            musicPlayer.UnPause();
+            Time.timeScale = 1;
+            inGame.enabled = true;
+            inGameMenu.enabled = false;
+            isEnabled = false;
+        }
+    }
 }
