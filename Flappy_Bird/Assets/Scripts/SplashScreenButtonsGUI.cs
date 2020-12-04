cb58b150461a7851ef847a4055dd529c511dbc77
Karen Nikoghosyan <63981352+KarenNikoghosyan@users.noreply.github.com>
2020-12-04 22:13:54 +0200

    V.0.70.3
    Fixed the mute button.
    Fixed the music volume slider.
    Now it saves the settings.

--- Flappy_Bird/Assets/Scripts/SplashScreenButtonsGUI.cs
+++ Flappy_Bird/Assets/Scripts/SplashScreenButtonsGUI.cs
@@ -4,24 +4,54 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using Michsky.UI.ModernUIPack;
+using TMPro;
 
 public class SplashScreenButtonsGUI : MonoBehaviour
 {
-    [SerializeField] Canvas splashButtons, quitMenu, settingsMenu;
+    [Header("Canvas")]
+    [SerializeField] Canvas splashButtons;
+    [SerializeField] Canvas quitMenu;
+    [SerializeField] Canvas settingsMenu;
+
+    [Header("transition Animation")]
     [SerializeField] GameObject transition;
     [SerializeField] float transitionTime = 2f;
-    [SerializeField] AudioSource audioSource;
+
+    [Header("Settings Menu")]
     [SerializeField] SliderManager sliderManager;
     [SerializeField] Button muteButton;
     [SerializeField] Sprite audioOff, audioOn;
-    [SerializeField] Camera audioListener;
+    [SerializeField] TextMeshProUGUI muteText;
+    
     bool audioSwitcher = true;
+    
+    AudioSource audioSource;
 
     void Start()
     {
-        //Adds a listener to the main slider and invokes a method when the value changes.
-        sliderManager.onValueChanged.AddListener(delegate { VolumeSlider(); });  
+        MusicVolumeSlider();
+        SoundSetting();
     }
+
+    private void MusicVolumeSlider()
+    {
+        //Adds a listener to the main slider and invokes a method when the value changes.
+        audioSource = GameObject.Find("Music Player").GetComponent<AudioSource>();
+        sliderManager.onValueChanged.AddListener(delegate { VolumeSlider(); });
+        //Gets the volume slider value and volume
+        audioSource.volume = PlayerPrefs.GetFloat("SliderVolume");
+        //Changes the slider value
+        sliderManager.mainSlider.value = PlayerPrefs.GetFloat("SliderVolume");
+    }
+
+    private void SoundSetting()
+    {
+        //Gets the mute or unmute setting
+        audioSwitcher = PlayerPrefs.GetInt("Mute SFX") == 1 ? true : false;
+        //Turns the sound on or off depends on the saved setting
+        MuteButton();
+    }
+
     public void StartGame()
     {
         AudioManager.instance.Play("Start Game Sound");
@@ -71,6 +101,8 @@ public class SplashScreenButtonsGUI : MonoBehaviour
     {
         settingsMenu.gameObject.SetActive(false);
         splashButtons.gameObject.SetActive(true);
+        //Saves the volume setting
+        PlayerPrefs.SetFloat("SliderVolume", audioSource.volume);
     }  
   
     public void VolumeSlider()
@@ -79,22 +111,37 @@ public class SplashScreenButtonsGUI : MonoBehaviour
         audioSource.volume = sliderManager.mainSlider.value;
     }
 
+    //Toggles the mute button
     public void MuteButton()
     {
         if (audioSwitcher)
         {
-            muteButton.GetComponent<Image>().sprite = audioOff;
-            audioListener.GetComponent<AudioListener>().enabled = false;
-            audioSource.Pause();
-            audioSwitcher = false;
+            MuteSound();
         }
 
         else
         {
-            muteButton.GetComponent<Image>().sprite = audioOn;
-            audioListener.GetComponent<AudioListener>().enabled = true;
-            audioSource.Play();
-            audioSwitcher = true;
+            UnmuteSound();
         }
+    }
+
+
+    private void MuteSound()
+    {
+        muteButton.GetComponent<Image>().sprite = audioOff;
+        muteText.text = "Unmute";
+        AudioListener.volume = 0f;
+        audioSwitcher = false;
+        //Saves the mute or unmute setting
+        PlayerPrefs.SetInt("Mute SFX", 1);
+    }
+    private void UnmuteSound()
+    {
+        muteButton.GetComponent<Image>().sprite = audioOn;
+        muteText.text = "Mute";
+        AudioListener.volume = 1f;
+        audioSwitcher = true;
+        //Saves the mute or unmute setting
+        PlayerPrefs.SetInt("Mute SFX", 0);
     }
 }
